d0a492ce284f1451384699a9046fbcee863a5334
Daneryl <daneryl@gmail.com>
2022-04-25 21:51:43 +0200

    reduced time of battery_info

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		        /*Update Interval*/	/*Update Signal*/
-	{"", "battery_info.sh",	                  60,	          	0},
+	{"", "battery_info.sh",	              20,	          	0},
 	{"", "volume_info.sh",	              0,	          	10},
 	{"", "cpu_info.sh",	                  5,	          	0},
 	{"", "date '+%b %d (%a) %I:%M%p'",		60,	          	0},
